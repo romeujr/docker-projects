bbeb7c21b95a263f4a4b8cb07b61b7449b03d8d4
Romeu Junior <romeu.junior@gmail.com>
2024-05-13 16:37:35 -0300

    - Work in progress;

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./build-image.sh romeujr/base_application dockerfile-base_application.txt '' '__=_'
+./build-image.sh '' romeujr/base_application ./dockerfile-base_application.txt '' '__=_'
+./build-image.sh 'romeujr/base_application' romeujr/nginx ./dockerfile-nginx.txt '' '__=_'
+./build-image.sh 'romeujr/base_application' romeujr/postgres ./dockerfile-postgres.txt '' '__=_'
--- build-image.sh
+++ build-image.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 BASE_IMAGE=$1
-DOCKER_FILE=$2
-EXTRA_PACKAGES=$3
-EXTRA_BUILD_ARGS=$4
+CURRENT_IMAGE=$2
+DOCKER_FILE=$3
+EXTRA_PACKAGES=$4
+EXTRA_BUILD_ARGS=$5
 
 echo BASE_IMAGE=$BASE_IMAGE
+echo CURRENT_IMAGE=$CURRENT_IMAGE
 echo DOCKER_FILE=$DOCKER_FILE
 echo EXTRA_PACKAGES=$EXTRA_PACKAGES
 echo EXTRA_BUILD_ARGS=$EXTRA_BUILD_ARGS
@@ -13,34 +15,34 @@ echo EXTRA_BUILD_ARGS=$EXTRA_BUILD_ARGS
 set +e
 
 echo "" && echo "Clean old images..."
-docker image rm $BASE_IMAGE:amd64
-docker image rm $BASE_IMAGE:arm64
+docker image rm $CURRENT_IMAGE:amd64
+docker image rm $CURRENT_IMAGE:arm64
 echo "y" | docker builder prune
 
 set -e
 
 echo "" && echo "Build for amd64..."
-docker buildx build --platform linux/amd64 --build-arg EXTRA_PACKAGES="$EXTRA_PACKAGES" --build-arg "$EXTRA_BUILD_ARGS" -t $BASE_IMAGE:amd64 -f $DOCKER_FILE .
+docker buildx build --platform linux/amd64 --build-arg BASE_IMAGE="$BASE_IMAGE":amd64 --build-arg EXTRA_PACKAGES="$EXTRA_PACKAGES" --build-arg "$EXTRA_BUILD_ARGS" -t $CURRENT_IMAGE:amd64 -f $DOCKER_FILE .
 
 echo "" && echo "Build for arm64..."
-docker buildx build --platform linux/arm64 --build-arg EXTRA_PACKAGES="$EXTRA_PACKAGES" --build-arg "$EXTRA_BUILD_ARGS" -t $BASE_IMAGE:arm64 -f $DOCKER_FILE .
+docker buildx build --platform linux/arm64 --build-arg BASE_IMAGE="$BASE_IMAGE":arm64 --build-arg EXTRA_PACKAGES="$EXTRA_PACKAGES" --build-arg "$EXTRA_BUILD_ARGS" -t $CURRENT_IMAGE:arm64 -f $DOCKER_FILE .
 
 echo "" && echo "Push for arm64..."
-docker push $BASE_IMAGE:arm64
+docker push $CURRENT_IMAGE:arm64
 
 echo "" && echo "Push for amd64..."
-docker push $BASE_IMAGE:amd64
+docker push $CURRENT_IMAGE:amd64
 
 set +e
 
 echo ""
 echo "Clean manifest."
-docker manifest rm $BASE_IMAGE:latest
+docker manifest rm $CURRENT_IMAGE:latest
 
 set -e
 
 echo "" && echo "Build manifest."
-docker manifest create $BASE_IMAGE:latest $BASE_IMAGE:amd64 $BASE_IMAGE:arm64
+docker manifest create $CURRENT_IMAGE:latest $CURRENT_IMAGE:amd64 $CURRENT_IMAGE:arm64
 
 echo "" && echo "Push manifest."
-docker manifest push $BASE_IMAGE:latest
+docker manifest push $CURRENT_IMAGE:latest
